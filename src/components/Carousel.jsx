8305a0cf163db735907c899a4cfb435348e28a34
pranavaselva <pranava.m@kalvium.community>
2024-02-05 13:37:23 +0530

    compeleted

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -8,6 +8,44 @@ import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 // complete the function below:
 function Carousel() {
+    const [count,setCount] = useState(0);
+    const data = { images };
+    const image = data.images[count].img;
+    const text1 = data.images[count].subtitle;
+    const text2 = data.images[count].title;
+    
+    const backward = () => {
+        if (count==2){
+            setCount(0)
+        }else{
+            setCount((cou) => {
+                return cou + 1
+            })
+        }
+    }
+
+    const forward = () => {
+        if(count==0){
+            setCount(2)
+        }else{
+            setCount((cou) => {
+                return cou - 1
+            })
+        }
+    }
+
+
+    return(
+        <div  className="container" >
+            <ArrowBackIosIcon className="arrow" onClick={forward}/>
+            <div className="container2" >
+            <h3 className="sub" >{text1}</h3>
+            <img src={image} alt="" />
+            <h3 className="title" >{text2}</h3>
+            </div>
+            <ArrowForwardIosIcon className="arrow" onClick={backward}/>            
+        </div>
+    )
 }
 
 export default Carousel;
